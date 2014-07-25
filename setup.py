60fbbe76e83ce1f7fc33c20319c665c6b59bd0d2
Daniel Lintott <daniel@serverb.co.uk>
2014-07-25 22:49:20 +0100

    Add classifiers to setup.py
    [ci-skip]

--- setup.py
+++ setup.py
@@ -13,5 +13,17 @@ setup(
     test_suite='tests',
     install_requires=['configobj'],
     package_data={'gns3converter': ['configspec']},
-    entry_points={'console_scripts': ['converter = gns3converter.main:main']}
+    entry_points={'console_scripts': ['converter = gns3converter.main:main']},
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Intended Audience :: Education',
+                 'Intended Audience :: Information Technology',
+                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Topic :: Education',
+                 'Topic :: Utilities']
 )
