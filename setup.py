e9553889577803cc66b1e19fe62115ad737fdb60
Daniel Lintott <daniel@serverb.co.uk>
2014-07-25 13:00:52 +0100

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='gns3-converter',
+    version='0.1',
+    packages=['gns3converter'],
+    url='https://github.com/dlintott/gns3-converter',
+    license='GNU General Public License v3 (GPLv3)',
+    author='Daniel Lintott',
+    author_email='daniel@serverb.co.uk',
+    description='<TODO>',
+    test_suite='tests',
+    requires=['configobj']
+)
