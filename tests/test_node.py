bb84756f7f978b12d19e0d85e05d94f94e7760d2
Daniel Lintott <daniel@serverb.co.uk>
2014-07-27 20:42:05 +0100

    Add tests for Node class

---
+++ tests/test_node.py
@@ -0,0 +1,23 @@
+import unittest
+from gns3converter.node import Node
+
+
+class TestNode(unittest.TestCase):
+    def setUp(self):
+        self.app = Node()
+        self.app.node_temp['ports'] = []
+
+    def test_add_wic(self):
+        exp_res = {'wic0': 'WIC-1T'}
+
+        self.app.add_wic('wic0/0', 'WIC-1T')
+        self.assertDictEqual(exp_res, self.app.node_temp_props)
+
+    def test_add_wic_ports(self):
+        exp_res = [{'name': 'Serial0/0',
+                    'id': 1,
+                    'port_number': 16,
+                    'slot_number': 0}]
+
+        self.app.add_wic_ports('WIC-1T', 0, 1)
+        self.assertDictEqual(exp_res[0], self.app.node_temp['ports'][0])
