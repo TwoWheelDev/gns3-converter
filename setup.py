7d481b642e5cbe414c8a6f9b00b787609c7b942b
Daniel Lintott <daniel@serverb.co.uk>
2014-08-02 11:42:16 +0100

    Use cx_Freeze to create a standalone executable for Windows
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+from cx_Freeze import setup, Executable
+import os.path
+
+zip_includes = [('gns3converter/configspec',
+                 os.path.join('gns3converter', 'configspec'))]
 
 setup(
     name='gns3-converter',
@@ -13,12 +18,20 @@ setup(
     test_suite='tests',
     install_requires=['configobj'],
     package_data={'gns3converter': ['configspec']},
-    entry_points={'console_scripts': ['converter = gns3converter.main:main']},
+    entry_points={'console_scripts':
+                ['converter = gns3converter.main:main']},
+    options={'build_exe': {'namespace_packages': 'gns3converter',
+                           'packages': 'gns3converter',
+                           'zip_includes': zip_includes
+                           }
+             },
+    executables=[Executable("gns3converter/main.py")],
     classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Console',
                  'Intended Audience :: Education',
                  'Intended Audience :: Information Technology',
-                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                 'License :: OSI Approved :: GNU General Public License '
+                 'v3 or later (GPLv3+)',
                  'Natural Language :: English',
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 3',
