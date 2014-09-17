5cfca468d9cc7da81f8e1cbaf462fb7aaba6c8d8
Daniel Lintott <daniel@serverb.co.uk>
2014-09-17 22:18:28 +0100

    Bump version to 0.3.4 and update changelog

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,7 +4,7 @@ import gns3converter
 
 class TestVersion(unittest.TestCase):
     def test_version(self):
-        self.assertEqual('0.3.3', gns3converter.__version__)
+        self.assertEqual('0.3.4', gns3converter.__version__)
 
 if __name__ == '__main__':
     unittest.main()
