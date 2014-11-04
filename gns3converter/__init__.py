e75292d6569e4101af41e53eea7db87ee474cefd
Daniel Lintott <daniel@serverb.co.uk>
2014-11-04 15:36:12 +0000

    Update changelog and release version 1.1.0

--- gns3converter/__init__.py
+++ gns3converter/__init__.py
@@ -12,4 +12,4 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-__version__ = '1.0.1-dev1'
+__version__ = '1.1.0'
