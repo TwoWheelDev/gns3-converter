3a9d5c0f99f15e4e90119fb3f99e43d2316beaca
Daniel Lintott <daniel@serverb.co.uk>
2015-03-09 17:40:22 +0000

    Bump version to 1.2.4.dev1

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,7 +4,7 @@ import gns3converter
 
 class TestVersion(unittest.TestCase):
     def test_version(self):
-        self.assertEqual('1.2.3', gns3converter.__version__)
+        self.assertEqual('1.2.4.dev1', gns3converter.__version__)
 
 if __name__ == '__main__':
     unittest.main()
